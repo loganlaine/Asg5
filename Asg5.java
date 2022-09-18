10fb97eecce746cccb67c747359e012afecbcece
Logan Laine <112809117+loganlaine@users.noreply.github.com>
2022-09-18 07:19:16 +0100

    Update Asg5.java

--- Asg5.java
+++ Asg5.java
@@ -16,7 +16,7 @@ public class Asg5 {
 		
 	}
 	
-	public static double convertCubicFeetToBushels(float cubicFeet)  {
+	public static double convertCubicFeetToBushels(double cubicFeet)  {
 		double BU = 0.803564;
 		double usBushels;
 		
@@ -25,69 +25,12 @@ public class Asg5 {
 		
 	}
 	
-	public static void cubicFeetToBushels() {
-		int cubicFeet = 0;
-		Scanner stdin = new Scanner(System.in);
-		
-		System.out.print("Enter cubic feet: ");
-		cubicFeet = stdin.nextInt();
-		double usBushels = convertCubicFeetToBushels(cubicFeet);
-
-		System.out.println();
-		System.out.println(cubicFeet + " cubic ft. = " + usBushels + " U.S. bushels");
-		
-		stdin.close();
-		
-	}
-	
-	public static double convertMilesToKilometers(double miles)  {
+	public static double convertMilesToKilometers(float miles)  {
 		double KM = 1.60934;
 		double kilometers;
 		
-		kilometers = miles * KM;
+		kilometers = (miles * KM);
 		return kilometers;
-		
-	}
-	
-	public static void milesToKilometers() {
-		int miles = 0;
-		Scanner stdin = new Scanner(System.in);
-		
-		System.out.print("Enter miles: ");
-		miles = stdin.nextInt();
-		double kilometers = convertMilesToKilometers(miles);
-		
-		System.out.println();
-		System.out.print(miles + " miles = " + kilometers + " km");
-		
-		stdin.close();
-		
-	}
-	
-	public static void graduationWithHonorsTitle() {
-		double gpa = 0;
-		Scanner stdin = new Scanner(System.in);
-		System.out.print("Enter GPA: ");
-		gpa = stdin.nextDouble();
-		
-		if (gpa >= 3.5 && gpa <= 3.7) {
-			System.out.print("Congratulations, you have graduated Cum Laude!");
-			
-		}
-		else if (gpa >= 3.8 && gpa <= 3.9) {
-			System.out.print("Congratulations, you have graduated Magna Cum Laude!");
-			
-		}
-		else if (gpa == 4.0){
-			System.out.print("Congratulations, you have graduated Summa Cum Laude!");
-			
-		}
-		else {
-			System.out.print("You did not graduate with honors.");
-			
-		}
-		
-		stdin.close();
 		
 	}
 
@@ -95,17 +38,65 @@ public class Asg5 {
 		
 		Scanner stdin = new Scanner(System.in);
 		int selection = 0;
+		float cubicFeet = 0;
+		float miles = 0;
 		
 		do {
-			
 			displayMenu();
-			//I am trying to clear the buffer with the line below but my buffer doenst seem to clear...
-			//stdin.nextLine();
+			
 			System.out.print("Enter your selection: ");
 			selection = stdin.nextInt();
-			
-			processChoice(selection);
 			System.out.println();
+			
+			if (selection == 1) {
+				
+				System.out.print("	Enter cubic feet: ");
+				cubicFeet = stdin.nextInt();
+				float usBushels = (float) convertCubicFeetToBushels(cubicFeet);
+				
+				
+				System.out.println("	" + cubicFeet + " cubic ft. = " + usBushels + " U.S. bushels");
+				System.out.println("");
+				
+			}
+			if (selection == 2) {
+								
+				System.out.print("	Enter miles: ");
+				miles = stdin.nextInt();
+				float kilometers = (float) convertMilesToKilometers(miles);
+				
+				
+				System.out.println("	" + miles + " mile(s) = " + kilometers + " km");
+				System.out.println("");
+				
+			}
+			if (selection == 3) {
+				double gpa = 0.0;
+				System.out.print("	Enter GPA: ");
+				gpa = stdin.nextDouble();
+				
+				if (gpa >= 3.5 && gpa <= 3.7) {
+					System.out.println("	Congratulations, you have graduated Cum Laude!");
+					System.out.println("");
+					
+				}
+				else if (gpa >= 3.8 && gpa <= 3.9) {
+					System.out.println("	Congratulations, you have graduated Magna Cum Laude!");
+					System.out.println("");
+					
+				}
+				else if (gpa == 4.0){
+					System.out.println("	Congratulations, you have graduated Summa Cum Laude!");
+					System.out.println("");
+					
+				}
+				else {
+					System.out.println("	You did not graduate with honors.");
+					System.out.println("");
+					
+				}
+				
+			}
 			
 		} while (selection != 4);
 		
@@ -113,8 +104,3 @@ public class Asg5 {
 	}
 	
 }
-
-
-
-
-
